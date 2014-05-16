a2847156e58ea06503d9ead2caed107b7bd1d0cb
Traun Leyden <traun@couchbase.com>
2014-05-16 09:36:04 -0700

    fix json unmarshal fallback behavior

--- ocr_engine.go
+++ ocr_engine.go
@@ -2,7 +2,6 @@ package ocrworker
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/couchbaselabs/logg"
 	"strings"
 )
@@ -30,41 +29,31 @@ func NewOcrEngine(engineType OcrEngineType) OcrEngine {
 
 func (e *OcrEngineType) UnmarshalJSON(b []byte) (err error) {
 
-	var object interface{}
+	var engineTypeStr string
 
-	if err := json.Unmarshal(b, &object); err != nil {
+	if err := json.Unmarshal(b, &engineTypeStr); err == nil {
+		logg.LogTo("OCR", "its a string")
+		engineString := strings.ToUpper(engineTypeStr)
+		switch engineString {
+		case "TESSERACT":
+			*e = ENGINE_TESSERACT
+		case "MOCK":
+			*e = ENGINE_MOCK
+		default:
+			logg.LogWarn("Unexpected OcrEngineType json: %v", engineString)
+			*e = ENGINE_MOCK
+		}
+		return nil
+	}
+
+	// not a string .. maybe it's an int
+
+	var engineTypeInt int
+	if err := json.Unmarshal(b, &engineTypeInt); err == nil {
+		*e = OcrEngineType(engineTypeInt)
+		return nil
+	} else {
 		return err
 	}
 
-	switch object.(type) {
-	case int, int8, int16, int32, int64:
-		if object, ok := object.(int); ok {
-			*e = OcrEngineType(object)
-		} else {
-			return fmt.Errorf("Error unmarshaling OcrEngineType, not int")
-		}
-
-	case string:
-		if object, ok := object.(string); ok {
-			engineString := strings.ToUpper(object)
-			switch engineString {
-			case "TESSERACT":
-				*e = ENGINE_TESSERACT
-			case "MOCK":
-				*e = ENGINE_MOCK
-			default:
-				logg.LogWarn("Unexpected OcrEngineType json: %v", engineString)
-				*e = ENGINE_MOCK
-			}
-
-		} else {
-			return fmt.Errorf("Error unmarshaling OcrEngineType, not string")
-		}
-
-	default:
-		logg.LogWarn("Got unexpected type OcrEngineType json: %T", object)
-		*e = ENGINE_MOCK
-	}
-
-	return
 }
