a5e09e9d7a8e5b59ee169a7700b6287d3d9bb3e2
Traun Leyden <tleyden@couchbase.com>
2014-10-29 09:06:19 -0700

    Getting it to run on CoreOS Fleet

--- docs/coreos-fleet/start-fleet.sh
+++ docs/coreos-fleet/start-fleet.sh
@@ -1,5 +1,11 @@
 
 # download all unit files
+wget https://raw.githubusercontent.com/tleyden/open-ocr/master/docs/coreos-fleet/httpd.service
+wget https://raw.githubusercontent.com/tleyden/open-ocr/master/docs/coreos-fleet/rabbitmq.service
+wget https://raw.githubusercontent.com/tleyden/open-ocr/master/docs/coreos-fleet/rabbitmq_announce.service
+wget https://raw.githubusercontent.com/tleyden/open-ocr/master/docs/coreos-fleet/worker.service
 
 # start all unit files
+echo "Starting units .."
+fleetctl start rabbitmq.service rabbitmq_announce.service httpd.service worker.service
 
