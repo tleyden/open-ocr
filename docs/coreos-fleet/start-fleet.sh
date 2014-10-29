505884f26bf4cdb4784619e8dae8938153dfadb0
Traun Leyden <tleyden@couchbase.com>
2014-10-29 09:35:41 -0700

    Start 3 workers

--- docs/coreos-fleet/start-fleet.sh
+++ docs/coreos-fleet/start-fleet.sh
@@ -5,7 +5,10 @@ wget https://raw.githubusercontent.com/tleyden/open-ocr/master/docs/coreos-fleet
 wget https://raw.githubusercontent.com/tleyden/open-ocr/master/docs/coreos-fleet/rabbitmq_announce.service
 wget https://raw.githubusercontent.com/tleyden/open-ocr/master/docs/coreos-fleet/worker.service
 
+# run 3 workers by default
+cp worker.service worker.1.service; cp worker.service worker.2.service; cp worker.service worker.3.service
+
 # start all unit files
 echo "Starting units .."
-fleetctl start rabbitmq.service rabbitmq_announce.service httpd.service worker.service
+fleetctl start rabbitmq.service rabbitmq_announce.service httpd.service worker.[0-9].service
 
